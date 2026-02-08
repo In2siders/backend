2646a82ca78862b7837ce5ce9be1fa5224130e1e
Ezequiel C. <e.cf@leiuq.fun>
2026-02-08 17:22:17 +0100

    yeah, I love womens

--- attachtments.py
+++ attachtments.py
@@ -29,15 +29,13 @@ def upload_base64_to_s3(base64_str, filename, chat_id):
         file_bytes = base64.b64decode(base64_str)
         file_obj = BytesIO(file_bytes)
 
-        # Generate the unique KEY (Path)
-        unique_name = f"{uuid.uuid4()}_{filename}"
+        unique_name = f"{str(uuid.uuid4()).replace('-', '')}_{filename}"
         s3_path = f"chats/{chat_id}/{unique_name}"
 
         # Upload to S3
         s3.upload_fileobj(file_obj, BUCKET_NAME, s3_path, ExtraArgs={"ACL": "private"})
 
         return s3_path
-
     except Exception as e:
         print(f"[-] S3 Upload Error: {e}")
         return None
@@ -46,9 +44,6 @@ def upload_base64_to_s3(base64_str, filename, chat_id):
 def get_signed_url(s3_key):
     if not s3_key:
         return None
-
-    if "amazonaws.com/" in s3_key:
-        s3_key = s3_key.split("amazonaws.com/")[-1].split("?")[0]
 
     try:
         url = s3.generate_presigned_url(
